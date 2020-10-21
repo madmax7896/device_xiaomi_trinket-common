b543456dfb0efc965f2d0ed0d9e4522ee9ae77a8
madmax7896 <thegreatsagar123@gmail.com>
2020-10-21 11:25:16 +0000

    trinket-common: Enable updatable apex

--- trinket.mk
+++ trinket.mk
@@ -13,6 +13,9 @@ $(call inherit-product, $(LOCAL_PATH)/utils.mk)
 # Vendor blobs
 $(call inherit-product, vendor/xiaomi/trinket-common/trinket-common-vendor.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
