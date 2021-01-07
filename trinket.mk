1c254381eb84774b44585e7741430d5cdd8dc3b0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-07 19:48:28 +0530

    trinket-common: Nuke Camera2

--- trinket.mk
+++ trinket.mk
@@ -84,8 +84,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    libdng_sdk.vendor \
-    Camera2
+    libdng_sdk.vendor
 
 # Context Hub
 PRODUCT_PACKAGES += \
